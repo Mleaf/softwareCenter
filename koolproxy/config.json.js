8593db0adc87ef83b8c0a2d4408216aa58e35bfd
mleaf <350983773@qq.com>
2019-06-27 15:03:58 +0800

    add config.json.js

---
+++ koolproxy/config.json.js
@@ -0,0 +1,10 @@
+{
+"build_date":"2019-06-27_14:55:21",
+"description":"广告屏蔽",
+"home_url":"Module_koolproxy.asp",
+"md5":"91aa6074bd1fde01249af9bd7812479d",
+"name":"koolproxyR",
+"tar_url": "http://www.mleaf.org/downloads/sbr-ac1200p/software/koolproxy/koolproxy.tar.gz", 
+"title":"koolproxyR",
+"version":"3.8.4"
+}
